091f807a63b598164f3154cc6a3806a9893d784d
thesomeexp <thesomeexp@riseup.net>
2020-03-26 23:24:47 +0800

    Singleton Pattern

---
+++ src/main/java/Singleton/Method2/Client.java
@@ -0,0 +1,15 @@
+package Singleton.Method2;
+
+import Singleton.Method2.pojo.Singleton;
+//客户端应用程序
+public class Client {
+	public static void main(String[] args) {
+		Singleton singleton1 = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+		if (singleton1 == singleton2) {
+			System.out.println("这是同一个对象");
+		} else {
+			System.out.println("这是不同对象");
+		}
+	}
+}
